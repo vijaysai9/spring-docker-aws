367beb5d2477c89ddd325f78cde36a617a24cf62
vijaysai9 <vijaysai.vankadaru@gmail.com>
2017-08-15 17:37:24 -0500

    Add files via upload

---
+++ springbootexample/src/main/java/com/example/Hello.java
@@ -0,0 +1,21 @@
+package com.example;
+
+
+public class Hello {
+
+
+    private String message;
+
+    public Hello(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
---
+++ springbootexample/src/main/java/com/example/HelloController.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+
+        @RequestMapping("/sayHello")
+        public Hello sayHello() {
+            return new Hello("Hello World");
+        }
+
+    }
